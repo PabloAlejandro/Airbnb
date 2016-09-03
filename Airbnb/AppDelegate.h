c04f8158866c7613305c0c6df5e891df766e2529
Paablo alejandro Pérez Martínez <pablo.alejandro.pm@gmail.com>
2016-09-03 09:58:29 +0100

    Initial Commit

---
+++ Airbnb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Airbnb
+//
+//  Created by Pau on 03/09/2016.
+//  Copyright © 2016 Pau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
